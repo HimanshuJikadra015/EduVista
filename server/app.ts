231fed63105b531bcca075e358ad09ea4c14e9ab
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-17 00:32:41 +0530

    Initialized Express app and configured body parser and cookie parser middleware

---
+++ server/app.ts
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors (cross origin resource sharing)
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// routes
+app.use("/api/v1", userRouter);
+
+app.use("/api/v1", courseRouter);
+
+// testing API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(errorMiddleware);
