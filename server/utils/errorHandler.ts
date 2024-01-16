6fb3b6be7733575bd646bffd84aa780495445a82
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-17 00:26:38 +0530

    Implemented custom error handler class

---
+++ server/utils/errorHandler.ts
@@ -0,0 +1,10 @@
+class errorHandler extends Error {
+  statusCode: Number;
+  constructor(message: any, statusCode: Number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default errorHandler;
