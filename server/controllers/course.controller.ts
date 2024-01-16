4697eb6914e38be293333a0e2e1533ba45658f1d
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-16 23:50:57 +0530

    Implemented upload, edit, and get single course functionalities

---
+++ server/controllers/course.controller.ts
@@ -0,0 +1,79 @@
+import { Request, Response, NextFunction } from "express";
+import { catchAsyncError } from "../middleware/catchAsyncErrors";
+import errorHandler from "../utils/errorHandler";
+import cloudinary from "cloudinary";
+import { createCourse } from "../services/course.service";
+import { url } from "inspector";
+import courseModel from "../models/course.model";
+
+// upload course
+export const uploadCourse = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      createCourse(data, res, next);
+    } catch (error: any) {
+      return next(new errorHandler(error.message, 500));
+    }
+  }
+);
+
+// edit course
+export const editCourse = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(data.thumbnail.public_id);
+
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      const courseId = req.params.id;
+
+      const course = await courseModel.findByIdAndUpdate(
+        courseId,
+        {
+          $set: data,
+        },
+        { new: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new errorHandler(error.message, 500));
+    }
+  }
+);
+
+// get single course (without purchasing)
+export const getSingleCourse = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const course = await courseModel.findById(req.params.id);
+    } catch (error: any) {
+      return next(new errorHandler(error.message, 500));
+    }
+  }
+);
