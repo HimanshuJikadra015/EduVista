5697d86afd7d95a9cba4e4222de39cde83153f84
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-18 23:27:30 +0530

    Added getAllCourses route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -20,6 +20,11 @@ courseRouter.put(
 courseRouter.get(
   "/get-course/:id",
   getSingleCourse
+);
+
+courseRouter.get(
+  "/get-courses",
+  getAllCourses
 );
 
 export default courseRouter;
