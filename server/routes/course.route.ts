f8b8df3eae70d8601139335934ac11abaa88a6f7
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-18 22:53:13 +0530

    Added getSingleCourse route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -15,6 +15,11 @@ courseRouter.put(
   isAuthenticated,
   authorizeRoles("admin"),
   editCourse
+);
+
+courseRouter.get(
+  "/get-course/:id",
+  getSingleCourse
 );
 
 export default courseRouter;
