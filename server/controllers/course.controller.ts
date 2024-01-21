5381ef0115be9b5b0eaed06f1b0053dc0c2d8412
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-21 17:01:59 +0530

    Added addAnswer functionality to add question reply

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -1,3 +1,4 @@
+/// <reference path="../@types/custom.d.ts" />
 import { Request, Response, NextFunction } from "express";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
 import errorHandler from "../utils/errorHandler";
@@ -7,6 +8,10 @@ import { url } from "inspector";
 import courseModel from "../models/course.model";
 import { redis } from "../utils/redis";
 import mongoose from "mongoose";
+import { title } from "process";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
 
 // upload course
 export const uploadCourse = catchAsyncError(
@@ -215,4 +220,86 @@ export const addQuestion = catchAsyncError(
       return next(new errorHandler(error.message, 500));
     }
   }
+);
+
+// add answer in course question
+interface IAddAnswerData {
+  answer: string;
+  courseId: string;
+  contentId: string;
+  questionId: string;
+}
+
+export const addAnswer = catchAsyncError(
+  catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { answer, courseId, contentId, questionId }: IAddAnswerData =
+        req.body;
+
+      const course = await courseModel.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new errorHandler("Invalid Content Id", 400));
+      }
+
+      const courseContent = course?.courseData?.find((item: any) =>
+        item._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        return next(new errorHandler("Invalid Content Id", 400));
+      }
+
+      const question = courseContent?.questions?.find((item: any) =>
+        item._id.equals(questionId)
+      );
+
+      if (!question) {
+        return next(new errorHandler("Invalid Question Id", 400));
+      }
+
+      // create a new answer object
+      const newAnswer: any = {
+        user: req.user,
+        answer,
+      };
+
+      // add this answer to course content
+      question.questionReplies.push(newAnswer);
+
+      await course?.save();
+
+      if (req.user?._id === question.user._id) {
+        // create a notification
+      } else {
+        const data = {
+          name: question.user.name,
+          title: courseContent.title,
+        };
+
+        const html = await ejs.renderFile(
+          path.join(__dirname, "../mails/question-reply.ejs"),
+          data
+        );
+
+        try {
+          await sendMail({
+            email: question.user.email,
+            subject: "New Reply to Your Question on EduVista",
+            template: "question-reply.ejs",
+            data,
+          });
+        } catch (error: any) {
+          return next(new errorHandler(error.message, 500));
+        }
+      }
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new errorHandler(error.message, 500));
+    }
+  })
 );
