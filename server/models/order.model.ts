a2eff4769cf939a2ed25f3ccc0a122fe5dfc04f5
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-01-23 23:22:10 +0530

    Defined orderSchema and IOrder interface

---
+++ server/models/order.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+  courseId: string;
+  userId: string;
+  paymentInfo: object;
+}
+
+const orderSchema = new Schema<IOrder>(
+  {
+    courseId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    paymentInfo: {
+      type: Object,
+      // required:true
+    },
+  },
+  { timestamps: true }
+);
+
+const orderModel: Model<IOrder> = mongoose.model("Order", orderSchema);
+export default orderModel;
