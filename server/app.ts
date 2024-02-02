163d3d9091e2aab16a9c283997bdebf0ff007e0b
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-02-02 18:59:06 +0530

    Added orderRouter to API routes

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -23,9 +24,7 @@ app.use(
 );
 
 // routes
-app.use("/api/v1", userRouter);
-
-app.use("/api/v1", courseRouter);
+app.use("/api/v1", userRouter, orderRouter, courseRouter);
 
 // testing API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
