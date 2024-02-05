12e8c889b7f586951fb312b93c9d0b1bcfa7f195
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-02-05 23:17:46 +0530

    Added updateUserRoleService service

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -24,3 +24,21 @@ export const getAllUsersService = async (res: Response) => {
     users,
   });
 };
+
+// update user role
+export const updateUserRoleService = async (
+  res: Response,
+  id: string,
+  role: string
+) => {
+  const users = await userModel.findByIdAndUpdate(
+    id,
+    { role },
+    { new: true }
+  );
+
+  res.status(201).json({
+    success: true,
+    users,
+  });
+};
