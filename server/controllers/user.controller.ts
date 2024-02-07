5aba06729cb42da16d066d7841d659ff7cbfe3cc
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-02-07 22:39:07 +0530

    Added deleteUser functionality

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -434,3 +434,28 @@ export const updateUserRole = catchAsyncError(
     }
   }
 );
+
+// delete user (only for admin)
+export const deleteUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const user = await userModel.findById(id);
+
+      if (!user) {
+        return next(new errorHandler("User not found", 404));
+      }
+
+      await user.deleteOne({ id });
+
+      await redis.del(id);
+
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+      });
+    } catch (error: any) {
+      next(new errorHandler(error.message, 400));
+    }
+  }
+);
