af91caf23b2321f7a893dca5387ff874cf44201d
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-02-07 23:41:43 +0530

    Added deleteCourse route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,7 @@ import {
   addQuestion,
   addReplyToReview,
   addReview,
+  deleteCourse,
   editCourse,
   getAllCourses,
   getAllCoursesAdmin,
@@ -52,6 +53,13 @@ courseRouter.get(
   isAuthenticated,
   authorizeRoles("admin"),
   getAllCoursesAdmin
+);
+
+courseRouter.delete(
+  "/delete-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  deleteCourse
 );
 
 export default courseRouter;
