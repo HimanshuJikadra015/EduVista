16e50a2185ce2b8ddb1842cf9afd5fa6bf85b95c
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-03-04 23:20:58 +0530

    Added layoutRouter to API routes

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,7 @@ import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -32,7 +33,8 @@ app.use(
   orderRouter,
   courseRouter,
   notificationRouter,
-  analyticsRouter
+  analyticsRouter,
+  layoutRouter
 );
 
 // testing API
