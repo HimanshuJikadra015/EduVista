9a0fad4271373ad0d726a90e55d0b5b76ff8ff54
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-03-06 23:20:13 +0530

    Added editLayout and getLayoutByType routes

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import {
+  createLayout,
+  editLayout,
+  getLayoutByType,
+} from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 
 layoutRouter.post(
@@ -9,5 +13,14 @@ layoutRouter.post(
   authorizeRoles("admin"),
   createLayout
 );
+
+layoutRouter.put(
+  "/edit-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editLayout
+);
+
+layoutRouter.get("/get-layout", getLayoutByType);
 
 export default layoutRouter;
