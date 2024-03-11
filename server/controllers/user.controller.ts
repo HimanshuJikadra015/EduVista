53f41b7a0c99e49b0f03d2443bad8ae27f18ad9b
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-03-11 19:18:07 +0530

    Update access token and set user session in Redis

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -207,7 +207,9 @@ export const updateAccessToken = catchAsyncError(
 
       const session = await redis.get(decoded.id as string);
       if (!session) {
-        return next(new errorHandler(message, 400));
+        return next(
+          new errorHandler("Please login to access this resource", 400)
+        );
       }
 
       const user = JSON.parse(session);
@@ -231,6 +233,8 @@ export const updateAccessToken = catchAsyncError(
 
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      await redis.set(user._id, JSON.stringify(user), "EX", 7 * 24 * 60 * 60); // 7 days
 
       res.status(200).json({
         status: "success",
