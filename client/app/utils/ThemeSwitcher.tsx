d54f69fb5d631ccd268730a81b8c0424af8675e5
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-03-18 22:30:12 +0530

    Added ThemeSwitcher component

---
+++ client/app/utils/ThemeSwitcher.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { BiMoon, BiSun } from "react-icons/bi";
+
+export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center justify-center mx-4">
+      {theme === "light" ? (
+        <BiMoon
+          className="curor-pointer"
+          fill="black"
+          size={25}
+          onClick={() => setTheme("dark")}
+        />
+      ) : (
+        <BiSun
+          className="curor-pointer"
+          size={25}
+          onClick={() => setTheme("light")}
+        />
+      )}
+    </div>
+  );
+};
