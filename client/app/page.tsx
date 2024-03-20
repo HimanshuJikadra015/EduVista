d2c4164874ad570b4522b8251e6ef98d81244631
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-03-20 23:23:10 +0530

    Added route state to Header component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,6 +8,8 @@ interface Props {}
 const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
+  const [route,setRoute] = useState("Login");
+  
   return (
     <div>
       <Heading
@@ -15,7 +17,7 @@ const Page: FC<Props> = (props) => {
         description="Empowering Students, Connecting Educators"
         keywords="Programming,MERN,Redux,Machine Learning, eLearning, Online Courses, Distance Learning, Educational Resources, Learning Management System (LMS), Study Material, Programming Courses, Mathematics Tutorials, Language Learning, Science Education, Arts and Humanities Courses, Student Platform, Virtual Classroom, Learning Community, Course Catalog"
       />
-      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} setRoute={setRoute} route={route}/>
       <Hero />
     </div>
   );
