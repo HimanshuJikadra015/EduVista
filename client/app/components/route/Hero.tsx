0e73ad653b9e8c6ae5302045d625021e27e974e2
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-07-12 01:25:19 +0530

    Updated Hero component

--- client/app/components/route/Hero.tsx
+++ client/app/components/route/Hero.tsx
@@ -9,12 +9,12 @@ const Hero: FC<Props> = (props) => {
   return (
     <div className="w-full 1000px:flex items-center">
       <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[50vh] left-5 w-[50vh] hero_animation rounded-[50%] 1100px:left-8 1500px:left-14"></div>
-      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 10000px:pt-[0] z-10">
+      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
         <Image
           src={require("../../../public/assets/banner-img-1.png")}
           alt=""
           priority
-          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-10"
+          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
         />
       </div>
       <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
