65fb4bff299386e7b6b444b518559f4c3cc2bfe3
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-07-12 01:17:44 +0530

    Updated page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,13 +3,14 @@ import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
 import Hero from "./components/route/Hero";
+
 interface Props {}
 
 const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
-  const [route,setRoute] = useState("Login");
-  
+  const [route, setRoute] = useState("Login");
+
   return (
     <div>
       <Heading
@@ -17,7 +18,13 @@ const Page: FC<Props> = (props) => {
         description="Empowering Students, Connecting Educators"
         keywords="Programming,MERN,Redux,Machine Learning, eLearning, Online Courses, Distance Learning, Educational Resources, Learning Management System (LMS), Study Material, Programming Courses, Mathematics Tutorials, Language Learning, Science Education, Arts and Humanities Courses, Student Platform, Virtual Classroom, Learning Community, Course Catalog"
       />
-      <Header open={open} setOpen={setOpen} activeItem={activeItem} setRoute={setRoute} route={route}/>
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
+      />
       <Hero />
     </div>
   );
