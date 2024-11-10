58a2a6beee985120757c91c971973c67d6f50f7e
HimanshuJikadra015 <himanshujikadra15@gmail.com>
2024-11-11 00:26:14 +0530

    Updated admin page layout with DashboardHero Component

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,28 +1,31 @@
 "use client";
-import React from 'react'
-import Heading from '../utils/Heading';
+import React from "react";
+import Heading from "../utils/Heading";
 import AdminSidebar from "../components/Admin/sidebar/AdminSidebar";
-import AdminProtected from '../hooks/adminProtected';
-type Props = {}
+import AdminProtected from "../hooks/adminProtected";
+import DashboardHero from "../components/Admin/DashboardHero";
+type Props = {};
 
 const page = (props: Props) => {
   return (
     <div>
-        <AdminProtected>
+      <AdminProtected>
         <Heading
           title="EduVista - Admin"
           description="Empowering Students, Connecting Educators"
           keywords="Programming,MERN,Redux,Machine Learning, eLearning, Online Courses, Distance Learning, Educational Resources, Learning Management System (LMS), Study Material, Programming Courses, Mathematics Tutorials, Language Learning, Science Education, Arts and Humanities Courses, Student Platform, Virtual Classroom, Learning Community, Course Catalog"
         />
         <div className="flex h-[200vh]">
-            <div className="1500px:w-[16%] w-1/5">
-                <AdminSidebar/>
-            </div>
-            <div className="w-[85%]"></div>
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero />
+          </div>
         </div>
-        </AdminProtected>
+      </AdminProtected>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
